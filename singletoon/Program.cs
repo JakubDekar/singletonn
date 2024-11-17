9f4935d9a135d8e7da7d96ae025ffe63565c81f8
JakubDekar <94068114+JakubDekar@users.noreply.github.com>
2024-11-17 17:43:15 +0100

    first commit

---
+++ singletoon/Program.cs
@@ -0,0 +1,59 @@
+﻿
+public class singleton
+{
+   
+    private static singleton instance;
+
+   
+    private static readonly object lockObject = new object();
+
+  
+    private singleton()
+    {
+        
+    }
+
+    
+    public static singleton GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = new singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+   
+    public void ShowMessage()
+    {
+        Console.WriteLine("instance");
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       
+        singleton singleton1 = singleton.GetInstance();
+        singleton1.ShowMessage();
+
+      
+        singleton singleton2 = singleton.GetInstance();
+        if (singleton1 == singleton2)
+        {
+            Console.WriteLine("Obě instance jsou stejné ");
+        }
+        else
+        {
+            Console.WriteLine("Instance jsou jiné");
+        }
+    }
+}
